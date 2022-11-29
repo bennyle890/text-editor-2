0dbf24b16dc4a2d45b7ebd01ef35ac9dbbf5e89b
Benny <benny.le890@gmail.com>
2022-11-28 22:03:11 -0500

    copied from origin repo to fix heroku deployment

---
+++ client/src-sw.js
@@ -0,0 +1,78 @@
+const { warmStrategyCache } = require('workbox-recipes');
+const { StaleWhileRevalidate, CacheFirst } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// TODO: Implement asset caching
+
+// const cacheName = 'static-resources';
+
+// Checks if resource is CSS or JS
+// const matchCallback = ({ request }) => {
+//   return (
+//     request.destination === 'style' || request.destination === 'script'
+//   )
+// }
+
+// CSS / JS are stashed here
+// registerRoute(matchCallback,
+//   new StaleWhileRevalidate({
+//     cacheName,
+//     plugins: [
+//   new CacheableResponsePlugin({
+//       statuses: [0, 200],
+//       }),
+//     ],
+//   })
+// );
+
+// Images are cached here
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination), // === 'image',
+  new StaleWhileRevalidate({
+    cacheName,
+    plugins: [
+  new CacheableResponsePlugin({
+      statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+  //   new CacheFirst({
+//     cacheName: 'my-image-cache',
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//       new ExpirationPlugin({
+//         maxEntries: 60,
+//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+//       }),
+//     ],
+//   })
+// );
+
---
+++ client/webpack.config.js
@@ -0,0 +1,80 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+// const WorkboxPlugin = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file. done
+// TODO: Add CSS loaders and babel to webpack. done
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // new WorkboxPlugin.GenerateSW(),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E.'
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just A Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'This is a text editor',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+          {
+            src: path.resolve('src/images/logo.png'),
+            size: '1024x1024',
+            destination: path.join('assets', 'icons'),
+            purpose: 'maskable'
+          }
+        ],
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(png|svg\jpg\jpeg\gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
